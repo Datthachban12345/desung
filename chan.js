311ad3ae2459a1e64156f2d49a58c16c087f61e2
Datthachban12345 <datthachban12345@gmail.com>
2024-09-27 13:27:11 +0700

    Change some texts

--- chan.js
+++ chan.js
@@ -26,7 +26,47 @@ var CART = {
             })
           })
     }
-}
+},
+  headerTop:function(){
+    btt = document.querySelector(".close-header");
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            console.log("hello");
+            btt.classList.add('show');
+        } else {
+          btt.classList.remove('show');
+        }
+      }
+  },
+  keyframe: function(){
+    document.addEventListener("DOMContentLoaded", function() {
+      // Lấy tất cả các phần tử cần được quan sát
+      const animatedBoxUp = document.querySelectorAll('.FadeInUp');
+      const animatedBoxRi = document.querySelectorAll('.FadeInRight');
+      const animatedBoxLe = document.querySelectorAll('.FadeInLeft');
+      // Khởi tạo Intersection Observer
+      const observer = new IntersectionObserver((entries, observer) => { // biến khai báo viewpoint có xuất hiện khung hình hay không 
+        entries.forEach(entry => {
+          // Kiểm tra nếu phần tử đang nằm trong viewport
+          if (entry.isIntersecting) {
+            entry.target.classList.add('wow');
+            observer.unobserve(entry.target);    // Ngừng quan sát phần tử này sau khi animation được kích hoạt
+          }
+        });
+      });
+      // Bắt đầu quan sát mỗi phần tử
+      animatedBoxUp.forEach(box => {
+        observer.observe(box);
+      });
+      animatedBoxLe.forEach(box => {
+        observer.observe(box);
+      });
+      animatedBoxRi.forEach(box => {
+        observer.observe(box);  //xác định viewpoint
+      });
+    });
+  },
 };
 var swiper = {
     banner:function(){
@@ -35,3 +75,5 @@ var swiper = {
 }
 CART.header();
 swiper.banner();
+CART.headerTop();
+CART.keyframe();
